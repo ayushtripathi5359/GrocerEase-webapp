5286c0e4e6e5e497e65a038181b18c49285d6660
ayushtripathi5359 <ayushtripathi5359@gmail.com>
2024-09-13 14:51:19 +0530

    new cors commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const { log } = require('console');
 
 
 app.use(express.json());
-app.use(cors());    //to connect from react frontend
+app.use(cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+  }));    //to connect from react frontend
 
 
 //DB connection with mongo DB
